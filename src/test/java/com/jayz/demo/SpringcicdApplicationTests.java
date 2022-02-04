365429f3a70b69946d06f08684a635edf9518208
Jay Jadhav <jadhav.jay0253@gmail.com>
2022-02-04 15:40:49 +0530

    buildspec added

---
+++ src/test/java/com/jayz/demo/SpringcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
